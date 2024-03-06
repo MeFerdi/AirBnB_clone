6adeda3fdb41caac6bf1a3da49ee75ae67db882b
MeFerdi <ferdinandodhis254@gmail.com>
2024-03-06 21:44:06 +0300

    update

---
+++ models/review.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Defines the Review class.
+"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """Represent a review
+
+    Attributes:
+        place_id (str): Defines Place id
+        user_id (str): Defines User id
+        text (str): Review text
+
+    """
+    place_id = ""
+    user_id = ""
+    text = ""
+    
